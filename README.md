ae8da211d9289a60dd97666bbc4ca97183b60f9f
dx0ne <nicholajs.borov@gmail.com>
2023-10-26 15:33:29 +0200

    electron and web working

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # emptyJSgame
 
+1. Initialize a New Node.js Project:
+Ensure you have Node.js and npm (Node Package Manager) installed
+npm init -y
+npm install pixi.js
+npm install lite-server --save-dev (or LiveServer for VS)
+
+npm start
 If you haven't set up a script, you can start lite-server directly with:
 npx lite-server
+
+npm install electron --save-dev
+
+1. Install electron-packager:
+# Install globally
+npm install electron-packager -g
+# Or as a dev dependency
+npm install electron-packager --save-dev
+
+2. Use electron-packager to Build the Executable:
+npx electron-packager . MyElectronApp --platform=win32 --arch=x64 --icon=path/to/your/icon.ico
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-let app = new PIXI.Application({ width: 800, height: 600 });
+let app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x1099bb,
+});
 document.body.appendChild(app.view);
 
-let rectangle = new PIXI.Graphics();
-rectangle.beginFill(0xff00FF);
-rectangle.drawRect(0, 0, 100, 100);
-rectangle.endFill();
+const stage = new PIXI.Container();
+app.stage.addChild(stage);
 
-app.stage.addChild(rectangle);
+let player = new PIXI.Graphics();
+player.beginFill(0xff00FF);
+player.drawRect(0, 0, 100, 100);
+player.endFill();
+player.pivot.set(player.width/2, player.height/2); // Set the pivot point to the center of the player
+stage.addChild(player);
+
+let keys = {};
+
+window.addEventListener('keydown', (e) => { keys[e.key] = true; });
+window.addEventListener('keyup', (e) => { keys[e.key] = false; });
+
+app.ticker.add(delta => gameLoop(delta));
+
+function gameLoop(delta) {
+    if (keys['w']) player.y -= 5;
+    if (keys['s']) player.y += 5;
+    if (keys['a']) player.x -= 5;
+    if (keys['d']) player.x += 5;
+   if (keys['Escape']) window.electronAPI.closeApp();
+}
+
+  window.addEventListener('resize', () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+});
